76852f42d175078b20c7a28431480c3ff0d97fa2
ali <alineamati.1381@gmail.com>
2022-10-07 19:04:53 +0330

    fonts & bootstrap & img added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-
   <router-view/>
 </template>
 
 <style lang="scss">
+@font-face {
+  font-family: main-font;
+  src: url('./assets/font/mediom.ttf');
+}
 body{
   margin: 0;
   font-size: 16px;
+  font-family: main-font;
 }
 </style>
