907db2e15bcadb361a1e6192169b1bba873ba2a8
ali <alineamati.1381@gmail.com>
2022-10-07 19:56:27 +0330

    header created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
+  <TopHeader />
   <router-view/>
 </template>
+
+<script>
+import TopHeader from '@/components/TopHeader.vue'
+export default {
+  components: { TopHeader }
+}
+</script>
 
 <style lang="scss">
 @font-face {
   font-family: main-font;
   src: url('./assets/font/mediom.ttf');
 }
-body{
+@font-face {
+  font-family: montserrat;
+  src: url('./assets/font/Montserrat-Regular.ttf');
+}
+body {
   margin: 0;
   font-size: 16px;
   font-family: main-font;
+}
+.english-text{
+  font-family: montserrat;
 }
 </style>
---
+++ src/components/TopHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand text-uppercase text-white" href="#">naturehomedec</a>
+      <button
+        class="navbar-toggler bg-light"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" dir="rtl" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto ms-1 mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a href="#" class="nav-link link-light ms-2">ارتباط با ما</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link link-light ms-2">درباره ما</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link link-light ms-2">وسایل </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link link-light ms-2">فروشگاه </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+</style>
